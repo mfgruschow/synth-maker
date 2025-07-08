73a633faf5d1377ebc91f1c54be3b01c2304d534
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2025-07-08 15:37:32 -0500

    setup base server

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+// Create a new express application instance
+const app = express();
+app.use(cors());
+
+// Set the network port
+const port = process.env.PORT || 3000;
+
+// Define the root path with a greeting message
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Welcome to the Express + TypeScript Server!" });
+});
+
+// Start the Express server
+app.listen(port, () => {
+    console.log(`The server is running at http://localhost:${port}`);
+});
